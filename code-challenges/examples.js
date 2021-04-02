1c8adec8cd92ac394936773af0b41ea3d663aed7
Terransky <nodeathking@gmail.com>
2021-04-02 16:45:01 -0700

    solved problems

---
+++ code-challenges/examples.js
@@ -0,0 +1,29 @@
+/*
+
+let fruits = ['apple', 'banana', 'cantaloupe', 'durian'];
+
+//arr.filter returns a boolean and uses methods or callbacks as parameters as a test
+console.log(fruits.filter( fruit => fruit.startsWith('b')));
+console.log(fruits.filter( fruit => fruit.endsWith('e')));
+console.log(fruits.filter( fruit => fruit.includes('a')));
+
+//typeof returns type as a string e.g.: typeof true = "boolean"
+
+const typeNum = arr => arr.filter(element => (typeof element) === 'number');
+const typeNum = arr => arr.filter(element => !isNaN(element));
+
+const containsAnd = (arr) => arr.filter(x => x.includes('and'));
+const containsAnd = (arr) => arr.filter(x => x.indexOf('and') !== -1);
+
+const getBaseStatGreaterThan = (arr, minBaseStat) => arr.filter(stat => stat.baseStat > minBaseStat);
+
+
+
+
+
+
+
+
+
+
+*/
